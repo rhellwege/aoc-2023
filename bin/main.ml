4832e01ed2d3e3b93e66f3359df2dd409c96f23f
Ryan Hellwege <rhellwege6@gmail.com>
2023-12-01 13:32:42 -0800

    convenience

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,7 @@
 open Aoc
 
 let () =
-  let arg: string = Array.get (Sys.argv) 1 in
+  if Array.length Sys.argv < 2 then print_endline "Please provide a file path as an argument."
+  else
+    let arg: string = Array.get (Sys.argv) 1 in
   Dayone.process arg |> print_int |> print_newline
