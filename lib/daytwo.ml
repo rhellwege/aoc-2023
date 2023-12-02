56230fb059b1c841d2abb0c0078dc9bfde6e9f17
Ryan Hellwege <rhellwege6@gmail.com>
2023-12-02 10:53:49 -0800

    2-1

--- lib/daytwo.ml
+++ lib/daytwo.ml
@@ -29,8 +29,7 @@ let parse_subgame line =
       | "blue" -> {acc with blue = n}
       | _ -> acc
       in
-      let comma = expect_string ", " line in
-      match comma with
+      match expect_string ", " line with
       | None -> Some (acc, line)
       | Some (_, line) -> aux acc line
     )
@@ -39,11 +38,32 @@ let parse_subgame line =
 
 let parse_game line =
   let (_, line) = get_opt @@ expect_string "Game " line in
-  let (_, line) = get_opt @@ parse_int line in
+  let (id, line) = get_opt @@ parse_int line in
   let (_, line) = get_opt @@ expect_string ": " line in
-  line
+  let rec aux acc line =
+    match parse_subgame line with
+    | None -> acc
+    | Some (sg, line) -> (
+        match expect_string "; " line with
+        | None -> sg :: acc
+        | Some (_, line) -> aux (sg :: acc) line
+      )
+  in 
+  let set = aux [] line in
+  {id = id; set = set}
+
+let game_possible (g: game) (original: subgame) : bool =
+  List.for_all (fun x -> 
+    x.red <= original.red && x.green <= original.green && x.blue <= original.blue
+  ) g.set
 
 let process infile = 
   let lines = Util.seq_of_filename infile in
-  let () = lines |> Seq.iter (fun line -> print_endline line;) in
-  1
+  lines 
+    |> Seq.filter_map (fun line -> 
+      match (line |> parse_game) with
+    | g when game_possible g {red=12;green=13;blue=14} -> Some g.id
+      | _ -> None
+    )
+    |> Seq.fold_left (fun sum id -> id + sum) 0
+
